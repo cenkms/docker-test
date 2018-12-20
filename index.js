2e4e8638960acadb4f9c1147e52847fd17d9ca94
Cenk Caglar <cecaglar@microsoft.com>
2018-12-20 17:35:19 -0500

    tabify!

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-//const http = require('http');
 const app = express();
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.get('/api', function (req, res) {
 	res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "msg": "hello api" }));
+	res.send(JSON.stringify({ "msg": "hello api" }));
 });
 
 app.listen(8080, function () {
-	  console.log('Example app listening on port 8080!');
+	console.log('Example app listening on port 8080!');
 });
