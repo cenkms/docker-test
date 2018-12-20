9916917b1acd79a56b933af8c8b86e00b02acd38
Cenk Caglar <cecaglar@microsoft.com>
2018-12-20 17:33:22 -0500

    updated the port and added an API

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+//const http = require('http');
 const app = express();
 
 app.get('/', function (req, res) {
 	  res.send('Hello World!');
 });
 
-app.listen(8000, function () {
-	  console.log('Example app listening on port 8000!');
+app.get('/api', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "msg": "hello api" }));
+});
+
+app.listen(8080, function () {
+	  console.log('Example app listening on port 8080!');
 });
